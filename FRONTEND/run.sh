8c54e8bfedee09d639df5c5fcbfa93de465d9487
lexxai <lexxai@gmail.com>
2024-04-06 18:11:25 +0300

    docker_app-first-run

--- FRONTEND/run.sh
+++ FRONTEND/run.sh
@@ -2,8 +2,12 @@
 
 echo Sleep 2 Waiting DB...
 sleep 2
-cd fastpakring
+ls -la *
+cd fastparking
 python manage.py migrate
 echo Sleep 2 Migration...
 sleep 2
 python manage.py runserver 0.0.0.0:8000
+
+#bash
+
--- scripts/docker_app.cmd
+++ scripts/docker_app.cmd
@@ -3,8 +3,6 @@
 PUSHD "..\deploy"
 ECHO "STOPPING SEPARATED DEV DB CONTAIMNINER"
 docker stop fastparking-db-postgres-1
-cd 
-dir
 rem docker-compose  --file docker-compose-project.yml --env-file .env  up -d 
-docker-compose  --file docker-compose-project.yml --env-file .env up 
+docker-compose  --file docker-compose-project.yml --env-file .env up  
 POPD
