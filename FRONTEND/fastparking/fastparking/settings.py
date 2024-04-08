00f32ebdeeb6e67e3317a4ec429bccf24b7a8775
RostislavB74 <brn.h74@gmail.com>
2024-04-08 16:00:11 +0300

    home

--- FRONTEND/fastparking/fastparking/settings.py
+++ FRONTEND/fastparking/fastparking/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "parking",
+    "accounts",
+    "communications",
+    "finance",
+    "images",
 ]
 
 MIDDLEWARE = [
