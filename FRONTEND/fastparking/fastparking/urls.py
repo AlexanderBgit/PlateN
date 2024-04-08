bbc944728f6a8d8ef60edce23fc58ed8605ee94f
RostislavB74 <brn.h74@gmail.com>
2024-04-08 20:45:40 +0300

    home

--- FRONTEND/fastparking/fastparking/urls.py
+++ FRONTEND/fastparking/fastparking/urls.py
@@ -22,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include ('parking.urls')),
-]
+    path("images/", include("images.urls"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
