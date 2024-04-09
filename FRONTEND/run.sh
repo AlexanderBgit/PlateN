31b29c5bed7ddc88bcb991e72db6ba441dd56f31
lexxai <lexxai@gmail.com>
2024-04-09 22:00:23 +0300

    cron loop run on docker on background

--- FRONTEND/run.sh
+++ FRONTEND/run.sh
@@ -4,9 +4,13 @@ echo Sleep 2 Waiting DB...
 sleep 2
 ls -la *
 cd fastparking
+echo RUN MIGRATION
 python manage.py migrate
 echo Sleep 2 Migration...
 sleep 2
+echo RUN BACKGROUND CRON-BOT
+./cron_loop.sh &
+echo RUN FRONTEND DJANGO
 python manage.py runserver 0.0.0.0:8000
 
 #bash
