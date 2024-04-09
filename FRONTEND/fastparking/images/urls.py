72603e9858c79558d13c4ecd3b2f81c097009844
RostislavB74 <brn.h74@gmail.com>
2024-04-09 16:27:30 +0300

    home

--- FRONTEND/fastparking/images/urls.py
+++ FRONTEND/fastparking/images/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name = "images"
 
-urlpatterns = [
-    path("", views.main, name='root'),
-    path("upload/", views.upload, name='upload'),
-    path("pictures/", views.pictures, name='pictures'),
-    path("pictures/edit/<int:pic_id>", views.edit, name='edit'),
-    path("pictures/remove/<int:pic_id>", views.remove, name='remove'),
-]
+# urlpatterns = [
+    
+#     path("upload/", views.upload, name='upload'),
+#     path("pictures/", views.pictures, name='pictures'),
+#     path("pictures/edit/<int:pic_id>", views.edit, name='edit'),
+#     path("pictures/remove/<int:pic_id>", views.remove, name='remove'),
+# ]
