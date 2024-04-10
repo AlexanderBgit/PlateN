80082ead50fb14caa02b4235cffc2ae806b79bbe
lexxai <lexxai@gmail.com>
2024-04-10 16:39:13 +0300

    add_argument

--- FRONTEND/fastparking/utils/sheduler.py
+++ FRONTEND/fastparking/utils/sheduler.py
@@ -1,5 +1,4 @@
 import time
-import datetime
 import argparse
 
 
@@ -7,23 +6,28 @@ from telegram_api import crone_pool
 
 
 def handler_telegram_pool():
-    print("handler_telegram_pool")
+    print("- handler_telegram_pool")
     crone_pool()
 
+
 def handler_limit_check():
-    print("handler_limit_check")
+    print("- handler_limit_check")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Infinite loop of service app events')
     parser.add_argument('--loop', action='store_true')
     parser.add_argument('--delay', type=int,  default=1, help='loop delay in seconds, default 1 sec') 
+    parser.add_argument('-t','--telegram_period', type=int,  default=2, help='Telegram bot pool period in seconds, default 2 sec') 
+    parser.add_argument('-l','--limit_period', type=int,  default=300, help='Limit Check period in seconds, default 300 sec') 
     args = parser.parse_args()
+
     if args.loop:
         loop_delay = args.delay
         time_now = time.time()
         periods = {
-            "telegram_pool" : 2,
-            "limit_check": 15
+            "telegram_pool" : args.telegram_period,
+            "limit_check": args.limit_period
         }
         actions = {
             "telegram_pool": handler_telegram_pool, 
