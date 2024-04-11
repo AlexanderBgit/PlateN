047fd92d3908152a60dfc90b3c645c34cf059500
lexxai <lexxai@gmail.com>
2024-04-11 05:16:13 +0300

    docker change order of stop

--- scripts/docker_app_stop.sh
+++ scripts/docker_app_stop.sh
@@ -2,7 +2,7 @@
 
 pushd "../deploy"
 echo "STOPPING SEPARATED DEV DB CONTAINER"
-docker stop fastparking-pg-1
 docker stop fastparking-code-1
+docker stop fastparking-pg-1
 docker-compose  --file docker-compose-project.yml --env-file .env down
 popd
