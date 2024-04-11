efcc9e99dd07ddfcbe474a53182a74c26b00466d
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2024-04-11 19:31:46 +0300

    change settings fastparking

--- FRONTEND/fastparking/fastparking/settings.py
+++ FRONTEND/fastparking/fastparking/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = "fastparking.wsgi.application"
 #     }
 # }
 
-POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "XXXXXXXX")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+# POSTGRES_DB = os.getenv("POSTGRES_DB", "fastparking")
+# POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+# POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "77766")
+# POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
+# POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 # print(f"{POSTGRES_DB=}")
 
