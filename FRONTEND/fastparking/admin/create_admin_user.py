eab01034d59c8b9c2339746b71e1c933497be655
lexxai <lexxai@gmail.com>
2024-04-12 01:59:05 +0300

    create_superuser by Django from .env

---
+++ FRONTEND/fastparking/admin/create_admin_user.py
@@ -0,0 +1,27 @@
+import os
+import django
+
+# from django.conf import settings
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastparking.settings")
+django.setup()
+
+DJANGO_SUPERUSER_USERNAME = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+DJANGO_SUPERUSER_PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "")
+DJANGO_SUPERUSER_EMAIL = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+if DJANGO_SUPERUSER_PASSWORD and DJANGO_SUPERUSER_EMAIL and DJANGO_SUPERUSER_USERNAME:
+    User = get_user_model()
+    try:
+        User.objects.create_superuser(
+            DJANGO_SUPERUSER_USERNAME, DJANGO_SUPERUSER_EMAIL, DJANGO_SUPERUSER_PASSWORD
+        )
+        # print(
+        #     f"Created admin {DJANGO_SUPERUSER_USERNAME=}, {DJANGO_SUPERUSER_EMAIL=}, {DJANGO_SUPERUSER_PASSWORD=}"
+        # )        
+    except Exception as err:
+        ...
+        # print("error", err)
+
+
