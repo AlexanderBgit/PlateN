308e9764e8a19042441c2ad39b8efd735b8bb350
plaha303 <111688671+plaha303@users.noreply.github.com>
2024-04-12 16:45:14 +0300

    publishing latest news

--- FRONTEND/fastparking/utils/get_news.py
+++ FRONTEND/fastparking/utils/get_news.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 STORE_FILE = Path(__file__).resolve().parent.joinpath("news.txt")
+TELEGRAM_CHANEL_FILE = 'telegram_channel.txt'
 
 
 def read_store():
@@ -28,6 +29,26 @@ def get_latest_block():
         return "Новин немає"
 
 
+def read_telegram_chanel():
+    if Path(TELEGRAM_CHANEL_FILE).exists():
+        with open(TELEGRAM_CHANEL_FILE, "r") as file:
+            return file.read().strip()
+    return None
+
+
+def write_telegram_chanel(content):
+    with open(TELEGRAM_CHANEL_FILE, "w") as file:
+        file.write(content)
+
+
+def publish_latest_news():
+    latest_news = get_latest_block()
+    last_published_news = read_telegram_chanel()
+
+    if latest_news != last_published_news:
+        write_telegram_chanel(latest_news)
+        print(latest_news)
+
+
 if __name__ == "__main__":
-    block = get_latest_block()
-    print(block)
+    publish_latest_news()
--- scripts/docker_db.sh
+++ scripts/docker_db.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 
 pushd "../deploy"
 docker-compose --file docker-compose-db.yml --env-file .env  up -d 
