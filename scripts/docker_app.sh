479d274821f247395115a7fc75599a2b31ac581c
lexxai <lexxai@gmail.com>
2024-04-12 05:46:22 +0300

    whereis docker-compose

--- scripts/docker_app.sh
+++ scripts/docker_app.sh
@@ -5,5 +5,8 @@ pushd "../deploy"
 # docker stop fastparking-db-postgres-1
 echo "STARTING"
 pwd
+env
+whereis docker-compose
 docker-compose  --file docker-compose-project.yml --env-file .env up -d
+
 popd
