39c03251493af5ef51a310a75aee89cc601f5bcb
lexxai <lexxai@gmail.com>
2024-04-12 06:06:29 +0300

    deploy

--- scripts/docker_app.sh
+++ scripts/docker_app.sh
@@ -4,9 +4,5 @@ pushd "../deploy"
 # echo "STOPPING SEPARATED DEV DB CONTAINER"
 # docker stop fastparking-db-postgres-1
 echo "STARTING"
-pwd
-env
-whereis docker-compose
 docker-compose  --file docker-compose-project.yml --env-file .env up -d
-
 popd
