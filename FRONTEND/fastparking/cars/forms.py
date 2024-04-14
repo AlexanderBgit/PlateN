cfbedbd2cfc7752311d6da3fe9349b4bc2b1a90e
AlexanderBgit <133023524+AlexanderBgit@users.noreply.github.com>
2024-04-14 17:19:56 +0300

    Update forms.py
    comment CarForm

--- FRONTEND/fastparking/cars/forms.py
+++ FRONTEND/fastparking/cars/forms.py
@@ -2,19 +2,19 @@
 from django import forms
 from .models import Car
 
-class CarForm(forms.ModelForm):
-    class Meta:
-        model = Car
-        fields = ['car_number', 'predict', 'blocked', 'pay_pass']  # Перерахуйте всі поля, які ви хочете відображати у формі
+# class CarForm(forms.ModelForm):
+#     class Meta:
+#         model = Car
+#         fields = ['car_number', 'predict', 'blocked', 'pay_pass']  # Перерахуйте всі поля, які ви хочете відображати у формі
 
-        widgets = {
-            'car_number': forms.TextInput(attrs={'class': 'form-control'}),
-            'photo_car': forms.FileInput(attrs={'class': 'form-control'}),
-            'predict': forms.NumberInput(attrs={'class': 'form-control'}),
-            'blocked': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            'pay_pass': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            # 'user': forms.Select(attrs={'class': 'form-select'})
-        }
+#         widgets = {
+#             'car_number': forms.TextInput(attrs={'class': 'form-control'}),
+#             'photo_car': forms.FileInput(attrs={'class': 'form-control'}),
+#             'predict': forms.NumberInput(attrs={'class': 'form-control'}),
+#             'blocked': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+#             'pay_pass': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+#             # 'user': forms.Select(attrs={'class': 'form-select'})
+#         }
 
 
 class MyCarForm(forms.ModelForm):  # додано другий такий самий клас
