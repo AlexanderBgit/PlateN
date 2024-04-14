378374faa826d3a0ef4ffd672386afd7ec9c5811
lexxai <lexxai@gmail.com>
2024-04-14 16:01:21 +0300

    try, except Registration.DoesNotExist

--- FRONTEND/fastparking/parking/repository.py
+++ FRONTEND/fastparking/parking/repository.py
@@ -10,11 +10,14 @@ class Registration:
 
 
 def find_registered_plate(num_auto: str, max_results: int = 1000) -> int | None:
-    reg = Registration.objects.get(
-        car_number_out__isnull=True, car_number_in__contains=num_auto
-    )
-    if reg:
-        return reg.id
+    try:
+        reg = Registration.objects.get(
+            car_number_out__isnull=True, car_number_in__contains=num_auto
+        )
+        if reg:
+            return reg.id
+    except Registration.DoesNotExist:
+        return None
 
     unclosed_registration = Registration.objects.filter(car_number_out__isnull=True)[
         :max_results
