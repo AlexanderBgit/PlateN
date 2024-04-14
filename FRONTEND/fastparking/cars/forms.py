3fe3cf32fdf810591f81aa43421a5583790e9591
AlexanderBgit <133023524+AlexanderBgit@users.noreply.github.com>
2024-04-14 17:09:57 +0300

    Update forms.py
    два класи для вирішення конфлікту

--- FRONTEND/fastparking/cars/forms.py
+++ FRONTEND/fastparking/cars/forms.py
@@ -15,3 +15,18 @@ class CarForm(forms.ModelForm):
             'pay_pass': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             # 'user': forms.Select(attrs={'class': 'form-select'})
         }
+
+
+class MyCarForm(forms.ModelForm):  # додано другий такий самий клас
+    class Meta:
+        model = Car
+        fields = ['car_number', 'predict', 'blocked', 'pay_pass']  # Перерахуйте всі поля, які ви хочете відображати у формі
+
+        widgets = {
+            'car_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'photo_car': forms.FileInput(attrs={'class': 'form-control'}),
+            'predict': forms.NumberInput(attrs={'class': 'form-control'}),
+            'blocked': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'pay_pass': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            # 'user': forms.Select(attrs={'class': 'form-select'})
+        }
