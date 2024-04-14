fe22e7697564a8f003c5114b08eac30be7c1dc7f
lexxai <lexxai@gmail.com>
2024-04-14 15:19:07 +0300

    communications: find_user_telegram_id

---
+++ FRONTEND/fastparking/communications/repository.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+
+
+def find_user_telegram_id(nickname: str) -> str | None:
+    User = get_user_model()
+    try:
+        result = User.objects.get(telegram_nickname=nickname)
+        return result.telegram_id
+    except User.DoesNotExist:
+        return None
+
+
+def save_user_telegram_id(nickname: str, telegram_id: srt) -> str | None:
+    User = get_user_model()
+    try:
+        result = User.objects.get(telegram_nickname=nickname)
+        result.telegram_id = telegram_id
+        result.save()
+        return result.id
+    except User.DoesNotExist:
+        return None
