a7dfbf8b91307b1f76962922f2c904ee60bd3ea3
lexxai <lexxai@gmail.com>
2024-04-14 15:57:19 +0300

    telegram_bot can save in DB telegram_id of /start as for @username as phone +380...

--- FRONTEND/fastparking/communications/repository.py
+++ FRONTEND/fastparking/communications/repository.py
@@ -1,7 +1,16 @@
 from django.contrib.auth import get_user_model
 
 
-def find_user_telegram_id(nickname: str) -> str | None:
+def find_user_by_telegram_id(telegram_id: str) -> str | None:
+    User = get_user_model()
+    try:
+        result = User.objects.get(telegram_id=telegram_id)
+        return result.telegram_nickname
+    except User.DoesNotExist:
+        return None
+
+
+def find_user_by_telegram_nickname(nickname: str) -> str | None:
     User = get_user_model()
     try:
         result = User.objects.get(telegram_nickname=nickname)
@@ -10,7 +19,7 @@ def find_user_telegram_id(nickname: str) -> str | None:
         return None
 
 
-def save_user_telegram_id(nickname: str, telegram_id: srt) -> str | None:
+def save_user_telegram_id(nickname: str, telegram_id: str) -> str | None:
     User = get_user_model()
     try:
         result = User.objects.get(telegram_nickname=nickname)
