d45827cd4dbe41be5db821354c5223b13f84b805
lexxai <lexxai@gmail.com>
2024-04-15 00:11:40 +0300

    script that create for docker container file git-version.txt

--- scripts/docker_app_build.cmd
+++ scripts/docker_app_build.cmd
@@ -1,6 +1,6 @@
 @echo off                                                                                                                                                   
 PUSHD "..\deploy"
-
+git rev-parse --short HEAD > ../FRONTEND/git-version.txt
 docker-compose  --file docker-compose-project.yml --env-file .env build code 
 rem timeout 1
 rem docker attach fastparking-code-1
