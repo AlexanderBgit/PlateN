7db880917cfec88b01146df7a1682e4ff2f4e50b
lexxai <lexxai@gmail.com>
2024-04-15 00:06:30 +0300

    script that create for docker container file git-version.txt

--- scripts/docker_app.cmd
+++ scripts/docker_app.cmd
@@ -3,6 +3,7 @@
 PUSHD "..\deploy"
 ECHO "STOPPING SEPARATED DEV DB CONTAINER"
 docker stop fastparking-db-postgres-1
+git rev-parse --short HEAD > ..\FRONTEND\git-version.txt
 rem docker-compose  --file docker-compose-project.yml --env-file .env  up -d 
 docker-compose  --file docker-compose-project.yml --env-file .env up  
 POPD
