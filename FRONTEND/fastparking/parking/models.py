fe4d48da23a059fa3c9baa5ce257fc31a0bc8407
AlexanderBgit <sb4mymail@gmail.com>
2024-04-15 01:09:30 +0300

    test report (html+models+view)

--- FRONTEND/fastparking/parking/models.py
+++ FRONTEND/fastparking/parking/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
 # Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class ParkingEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    entry_time = models.DateTimeField()
+    exit_time = models.DateTimeField()
+    car_number = models.CharField(max_length=20)
+    parking_cost = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.car_number} - {self.entry_time} to {self.exit_time}"
--- FRONTEND/fastparking/parking/views.py
+++ FRONTEND/fastparking/parking/views.py
@@ -10,3 +10,18 @@ def main(request):
         "parking/index.html",
         {"title": "Fast Parking", "active_menu": active_menu},
     )
+
+from parking.models import ParkingEntry
+from .models import Sessions
+
+def generate_report(request):
+    user = request.user
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    entries = Sessions.objects.all()[:10]
+
+    parking_entries = ParkingEntry.objects.filter(user=user, 
+                                                  entry_time__range=[start_date, end_date])
+
+    return render(request, 'accounts/report.html', {'entries': entries, 'start_date': start_date, 'end_date': end_date})
+
