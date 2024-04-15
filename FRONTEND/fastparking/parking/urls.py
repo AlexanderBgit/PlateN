57f1975f1c86ed551636e79ecb5cf79c247d12ce
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2024-04-15 22:21:56 +0300

    Update urls.py

--- FRONTEND/fastparking/parking/urls.py
+++ FRONTEND/fastparking/parking/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'parking'
 
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
     path('parking_plan/', views.parking_plan_view, name='parking_plan'),
     path('registration/', views.registration_list, name='registration_list'),
    
