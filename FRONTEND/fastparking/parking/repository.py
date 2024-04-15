f0d86a03f9a4752e83ed5e8b71c0dacc510442ef
RostislavB74 <brn.h74@gmail.com>
2024-04-15 22:13:59 +0300

    reg

---
+++ FRONTEND/fastparking/parking/repository.py
@@ -0,0 +1,33 @@
+# from .models import Registration
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
+
+from .services import compare_plates
+from .models import Registration
+
+
+def get_registration_instance(id: int) -> Registration | None:
+    try:
+        return Registration.objects.get(pk=id)
+    except ObjectDoesNotExist:
+        return None
+
+
+def find_registered_plate(num_auto: str, max_results: int = 1000) -> int | None:
+    try:
+        reg = Registration.objects.get(
+            car_number_out__isnull=True, car_number_in__contains=num_auto
+        )
+        if reg:
+            return reg.id
+    except Registration.DoesNotExist:
+        return None
+
+    unclosed_registration = Registration.objects.filter(car_number_out__isnull=True)[
+        :max_results
+    ]
+    for reg in unclosed_registration:
+        reg_num_auto = reg.car_number_in
+        result, sim = compare_plates(num_auto, reg_num_auto)
+        if result:
+            return reg.id
