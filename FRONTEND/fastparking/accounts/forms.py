31ea0e464817f3d5241338babb2e8f8ad6e9ee00
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2024-04-16 19:44:25 +0300

    add edit profile, add change password

--- FRONTEND/fastparking/accounts/forms.py
+++ FRONTEND/fastparking/accounts/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm, CharField
+from django.contrib.auth.forms import PasswordChangeForm
 from django import forms
+
 from .models import MyCars
 from cars.models import Car
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm
+
+User = get_user_model()
 
 
 class MyCarsForm(ModelForm):
@@ -24,3 +30,48 @@ class CarNumberForm(ModelForm):
         model = Car
         fields = ["car_number"]
         exclude = ["user", "photo_car", "predict", "blocked", "pay_pass"] 
+
+class EditForm(UserChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('accept_oferta', None)
+        self.fields.pop('password', None)
+
+
+    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control"}))
+    first_name = CharField(max_length=150, required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = CharField(max_length=150, required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    email = forms.EmailField(max_length=150, required=True, widget=forms.EmailInput(attrs={"class": "form-control"}))
+    phone_number = forms.CharField(max_length=20, required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    accept_oferta = forms.BooleanField(required=True)
+    telegram_nickname = forms.CharField(max_length=20, required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
+    
+    class Meta:
+        model = User 
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email', 
+            'phone_number',
+            'telegram_nickname',             
+        )
+        exclude = ["telegram_id", "password1", "password2"] 
+
+class EditPassword(PasswordChangeForm):
+    
+    class Meta:
+        model = User 
+        fields = ['password1', 'password2']
+        exclude = [
+            'username',
+            'first_name',
+            'last_name',
+            'email', 
+            'phone_number',
+            'telegram_nickname', 
+            ] 
+
+
+    
+
