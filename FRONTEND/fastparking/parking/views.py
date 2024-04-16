7a8163f05e592cab61d79f1d5d2d3da00d72c548
lexxai <lexxai@gmail.com>
2024-04-17 02:43:04 +0300

    parking_plan

--- FRONTEND/fastparking/parking/views.py
+++ FRONTEND/fastparking/parking/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .models import Registration
 from .models import ParkingSpace
 
+
 def main(request):
     total_parking_spaces = settings.PARKING_SPACES_COUNT
     occupied_parking_spaces = ParkingSpace.objects.filter(status=True).count()
@@ -22,73 +23,106 @@ def main(request):
         },
     )
 
+
 def generate_report(request):
+    active_menu = "home"
     user = request.user
-    entry_datetime = request.GET.get('start_date')
-    exit_datetime = request.GET.get('end_date')
+    entry_datetime = request.GET.get("start_date")
+    exit_datetime = request.GET.get("end_date")
     car = car
-    
 
-    parking_entries = Registration.objects.filter(user=user, 
-                                                  entry_time__range=[entry_datetime, exit_datetime])
+    parking_entries = Registration.objects.filter(
+        user=user, entry_time__range=[entry_datetime, exit_datetime]
+    )
 
-    return render(request, 'accounts/report.html', {'car': car, 'start_date': entry_datetime, 'end_date': exit_datetime})
+    return render(
+        request,
+        "accounts/report.html",
+        {
+            "title": "Report",
+            "active_menu": active_menu,
+            "car": car,
+            "start_date": entry_datetime,
+            "end_date": exit_datetime,
+            "entries": parking_entries,
+        },
+    )
+
 
 def parking_plan_view(request):
+    active_menu = "home"
     parking_spaces = ParkingSpace.objects.all()
+    parking_spaces_count = parking_spaces.filter(status=False).count()
 
     # Розбиття місць на рядки
-    row_length = 10  # Довжина рядка (кількість місць у рядку)
-    parking_rows = [parking_spaces[i:i+row_length] for i in range(0, len(parking_spaces), row_length)]
+    # row_length = 10  # Довжина рядка (кількість місць у рядку)
+    # parking_rows = [
+    #     parking_spaces[i : i + row_length]
+    #     for i in range(0, len(parking_spaces), row_length)
+    # ]
+    content = {
+        "title": "Parking Plan",
+        "active_menu": active_menu,
+        "parking_spaces": parking_spaces,
+        "parking_spaces_count": parking_spaces_count,
+    }
+    return render(request, "parking/parking_plan.html", content)
 
-    return render(request, 'parking/parking_plan.html', {'parking_rows': parking_rows})
 
 def registration_list(request):
+    active_menu = "registration"
     registrations = Registration.objects.all()
-    return render(request, 'parking/registration_list.html', {'registrations': registrations})
+    return render(
+        request, "parking/registration_list.html", {"registrations": registrations}
+    )
 
 
 def entry_registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Отримання даних з форми
-        parking_id = request.POST.get('parking_id')
-        car_number_in = request.POST.get('car_number_in')
-        
+        parking_id = request.POST.get("parking_id")
+        car_number_in = request.POST.get("car_number_in")
+
         # Створення реєстрації заїзду
         parking = ParkingSpace.objects.get(id=parking_id)
-        entry_registration = EntryRegistration.objects.create(parking=parking, car_number_in=car_number_in)
-        
+        entry_registration = EntryRegistration.objects.create(
+            parking=parking, car_number_in=car_number_in
+        )
+
         # Перенаправлення на сторінку з реєстрацією виїзду
-        return redirect('exit_registration', entry_id=entry_registration.id)
-    
-    return render(request, 'entry_registration_form.html')
+        return redirect("exit_registration", entry_id=entry_registration.id)
+
+    return render(request, "entry_registration_form.html")
+
 
 def exit_registration(request, entry_id):
-    if request.method == 'POST':
+    if request.method == "POST":
         # Отримання даних з форми
         exit_datetime = timezone.now()
-        car_number_out = request.POST.get('car_number_out')
-        
+        car_number_out = request.POST.get("car_number_out")
+
         # Отримання реєстрації заїзду
         entry_registration = EntryRegistration.objects.get(id=entry_id)
-        
+
         # Створення реєстрації виїзду
         exit_registration = ExitRegistration.objects.create(
             parking=entry_registration.parking,
             entry_registration=entry_registration,
             exit_datetime=exit_datetime,
-            car_number_out=car_number_out
+            car_number_out=car_number_out,
         )
-        
-        # Створення об'єднаної реєстрації
-        combined_registration = CombinedRegistration.create_combined_registration(entry_registration, exit_registration)
-        
-        # Перенаправлення на іншу сторінку
-        return redirect('some_other_view')
 
-    return render(request, 'exit_registration_form.html')
+        # Створення об'єднаної реєстрації
+        combined_registration = CombinedRegistration.create_combined_registration(
+            entry_registration, exit_registration
+        )
+
+        # Перенаправлення на іншу сторінку
+        return redirect("some_other_view")
+
+    return render(request, "exit_registration_form.html")
 
 
 def registration_table(request):
     registrations = EntryRegistration.objects.all()
-    return render(request, 'registration_table.html', {'registrations': registrations})
+    return render(request, "registration_table.html", {"registrations": registrations})
