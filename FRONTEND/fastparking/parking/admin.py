0508bdb0db8225224d30b588c30c9ddd0c5ab37a
lexxai <lexxai@gmail.com>
2024-04-16 05:07:58 +0300

    admin ParkingSpace, Registration

--- FRONTEND/fastparking/parking/admin.py
+++ FRONTEND/fastparking/parking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ParkingSpace, Registration
+
+admin.site.register(ParkingSpace)
+admin.site.register(Registration)
