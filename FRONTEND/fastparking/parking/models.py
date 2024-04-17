55ae0b3451b53afe7b143b873a1b22aee131e30b
RostislavB74 <brn.h74@gmail.com>
2024-04-17 15:50:59 +0300

    add tariffs

--- FRONTEND/fastparking/parking/models.py
+++ FRONTEND/fastparking/parking/models.py
@@ -1,15 +1,6 @@
-import datetime
-
-from django.contrib.auth.models import User
-
 from django.db import models
-from photos.models import Photo
-from django.db import models
-from photos.models import Photo
 from cars.models import Car
-
-# from finance.repository import calculate_current_invoice
-# import finance.repository as finance_repo
+from photos.models import Photo
 
 
 class ParkingSpace(models.Model):
@@ -22,11 +13,11 @@ class ParkingSpace(models.Model):
     def __str__(self):
         return self.number
 
-
 class Registration(models.Model):
     parking = models.ForeignKey(ParkingSpace, on_delete=models.CASCADE)
     entry_datetime = models.DateTimeField(auto_now_add=True)
     car_number_in = models.CharField(max_length=16)
+    tariff_in = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  # Змінено поле на DecimalField
     exit_datetime = models.DateTimeField(null=True, blank=True)
     invoice = models.CharField(max_length=255, null=True, blank=True)
     car_number_out = models.CharField(max_length=16, null=True, blank=True)
@@ -45,7 +36,7 @@ class Registration(models.Model):
         blank=True,
     )
     car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True, blank=True)
-
+    
     def __str__(self):
         if self.invoice:
             invoice_predict = self.invoice
@@ -54,3 +45,5 @@ class Registration(models.Model):
             # invoice_predict = finance_repo.calculate_current_invoice(self.id)
         e_date = self.entry_datetime.strftime("%Y-%m-%d %H:%M")
         return f"Registration ID: {self.id:06} - Parking Number: {self.parking.number} - Entry: {e_date} - Invoice*: {invoice_predict}"
+    
+    
