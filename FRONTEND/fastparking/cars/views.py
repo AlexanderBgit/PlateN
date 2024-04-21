fb10e6153b671d113476a719c63bc76c90bfb1c4
lexxai <lexxai@gmail.com>
2024-04-22 01:39:49 +0300

    cars paginator

--- FRONTEND/fastparking/cars/views.py
+++ FRONTEND/fastparking/cars/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from cars.models import Car
 
 # from .forms import MyCarForm, CarNumberForm
@@ -5,26 +6,48 @@ from django.views.generic import ListView
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
 
 
 class CarListView(ListView):
     model = Car
     template_name = "car_list.html"  # Шаблон для відображення списку автомобілів
     context_object_name = "cars"  # Ім'я змінної в контексті шаблону
+    page_items = settings.PAGE_ITEMS
+
+    def validate_int(self, value: str | int | None) -> int | None:
+        if value is not None:
+            try:
+                value = int(value)
+            except (TypeError, ValueError):
+                value = 1
+            if value < 1:
+                value = 1
+        return value
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get the original context
         context = super().get_context_data(**kwargs)
+        page_number = self.validate_int(self.request.GET.get("page"))
+        queryset = context['object_list']
+        paginator = Paginator(queryset, self.page_items)
+        if page_number:
+            page_obj = paginator.get_page(page_number)
+        else:
+            page_obj = paginator.page(1)  # Get the first page by default
+
+        # Call the base implementation first to get the original context
 
         # Add additional context data
         context["title"] = "Cars"
         context["active_menu"] = "cars"
+        context["paginator"] = paginator
+        context["page_obj"] = page_obj
 
         return context
 
     def get_queryset(self):
-        return (
-            Car.objects.all()
+        return Car.objects.all().order_by(
+            "car_number",
         )  # Return your queryset dynamically based on your requirements
 
     def post(self, request, *args, **kwargs):
