0a00e00265f4ad84df03e25cc2206c2e7e71000c
lexxai <lexxai@gmail.com>
2024-04-23 20:07:09 +0300

    tune tariff input fields w/o seconds, and minimum is previos tariff + 1 min

--- FRONTEND/fastparking/finance/forms.py
+++ FRONTEND/fastparking/finance/forms.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
+import pytz
 from django import forms
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.utils import timezone
 
 from .models import Tariff, Payment, Registration
 from parking.repository import get_registration_instance
@@ -30,6 +34,28 @@ class TariffForm(forms.ModelForm):
                 attrs={"type": "datetime-local", "class": "form-control"}
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        end_date_default = "2999-12-31 23:59"
+        self.initial["end_date"] = datetime.strptime(end_date_default, "%Y-%m-%d %H:%M")
+        latest_object = self.Meta.model.objects.all().order_by("-start_date").first()
+        if latest_object:
+            min_date = latest_object.start_date
+            min_date += timezone.timedelta(minutes=1)
+            self.fields["start_date"].widget.attrs["min"] = min_date.strftime(
+                "%Y-%m-%dT%H:%M"
+            )
+            self.fields["start_date"].widget.attrs[
+                "title"
+            ] = f'Minimum : {min_date.strftime("%Y-%m-%d %H:%M")}'
+
+    def clean_end_date(self):
+        end_date: datetime = self.cleaned_data.get("end_date")
+        if end_date and end_date.minute == 59:
+            end_date = end_date.replace(second=59)
+        print(f"clean_end_date: {end_date=}")
+        return end_date
 
 
 class PaymentsForm(forms.ModelForm):
