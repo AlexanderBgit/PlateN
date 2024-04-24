ca86f5ce256a56f31d3ecb32fac1377930182dab
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2024-04-24 21:13:42 +0300

    fix users sinup

--- FRONTEND/fastparking/users/models.py
+++ FRONTEND/fastparking/users/models.py
@@ -1,15 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from cars.models import Car
 
 
+def validate_unique_telegram_nickname(value):
+    if value:
+        existing_users = CustomUser.objects.filter(telegram_nickname=value)
+        if existing_users.exists():
+            raise ValidationError(
+                _('This telegram nickname is already in use.'),
+                code='invalid'
+            )
+   
+
+
 class CustomUser(AbstractUser):
-    # cars = models.TextField(blank=True, null=True)
-    telegram_nickname = models.CharField(max_length=20, unique=True, blank=True, null=True)
+    telegram_nickname = models.CharField(max_length=20, blank=True, null=True, validators=[validate_unique_telegram_nickname])
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     telegram_id = models.CharField(max_length=50, blank=True, null=True)
-    cars = models.ManyToManyField(Car, related_name='owners', blank=True)  # Додано поле багато-до-багатьох для прив'язки автомобілів до користувача
+    cars = models.ManyToManyField(Car, related_name='owners', blank=True)  
     def __str__(self):
         return self.username
