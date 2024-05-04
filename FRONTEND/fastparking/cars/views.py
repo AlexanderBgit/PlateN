04d085cdb6a36acba0b052d38a218eb4028aed30
lexxai <lexxai@gmail.com>
2024-05-04 19:31:53 +0300

    car list tune filter form

--- FRONTEND/fastparking/cars/views.py
+++ FRONTEND/fastparking/cars/views.py
@@ -31,43 +31,56 @@ class CarListView(SuperuserRequiredMixin, ListView):
                 value = 1
         return value
 
+    def get_filter_params(self) -> dict:
+        page_number = self.validate_int(self.request.GET.get("page"))
+        car_no = self.request.GET.get("car_no", "")
+        blocked = self.request.GET.get("blocked")
+        paypass = self.request.GET.get("paypass")
+        if blocked:  # Handle potential absence of 'blocked' parameter
+            blocked = blocked.strip().lower() == "true"
+        if paypass:  # Handle potential absence of 'blocked' parameter
+            paypass = paypass.strip().lower() == "true"
+        if car_no:
+            car_no = car_no.strip().upper()
+            car_no = "".join(char for char in car_no if char.isalnum())
+        filter_params = {
+            "car_no": car_no,
+            "blocked": blocked,
+            "paypass": paypass,
+            "page": page_number,
+        }
+        return filter_params
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        page_number = self.validate_int(self.request.GET.get("page"))
+        filter_params = self.get_filter_params()
         queryset = context["object_list"]
         paginator = Paginator(queryset, self.page_items)
-        if page_number:
-            page_obj = paginator.get_page(page_number)
+        if filter_params.get("page"):
+            page_obj = paginator.get_page(filter_params["page"])
         else:
             page_obj = paginator.page(1)  # Get the first page by default
-
-        # Call the base implementation first to get the original context
 
         # Add additional context data
         context["title"] = "Cars"
         context["active_menu"] = "cars"
         context["paginator"] = paginator
         context["page_obj"] = page_obj
+        context["filter_params"] = filter_params
 
         return context
 
     def get_queryset(self):
         queryset = Car.objects.all().order_by("car_number")
-        car_number = self.request.GET.get("car_number")
-        blocked = self.request.GET.get("blocked")
-        paypass = self.request.GET.get("paypass")
-
-        if car_number:
-            car_number = car_number.strip().upper()
-            car_number = "".join(char for char in car_number if char.isalnum())
-            queryset = queryset.filter(car_number__icontains=car_number)
-        if blocked:  # Handle potential absence of 'blocked' parameter
-            blocked = blocked.strip().lower() == "true"
-            queryset = queryset.filter(blocked=blocked)
-        if paypass:  # Handle potential absence of 'blocked' parameter
-            paypass = paypass.strip().lower() == "true"
-            queryset = queryset.filter(PayPass=paypass)
-
+        filter_params = self.get_filter_params()
+        if filter_params.get("car_no"):
+            queryset = queryset.filter(car_number__icontains=filter_params["car_no"])
+        if filter_params.get(
+            "blocked"
+        ):  # Handle potential absence of 'blocked' parameter
+            queryset = queryset.filter(blocked=filter_params["blocked"])
+        if filter_params.get("paypass"):
+            queryset = queryset.filter(PayPass=filter_params["paypass"])
         return queryset
 
     def post(self, request, *args, **kwargs):
@@ -75,12 +88,14 @@ class CarListView(SuperuserRequiredMixin, ListView):
         blocked_id = request.POST.getlist("blocked")
         pay_pass_id = request.POST.getlist("pay_pass")
         for id in cars_id:
-            blocked = id in blocked_id
-            pay_pass = id in pay_pass_id
-            # print(id, blocked, pay_pass)
-            try:
-                Car.objects.filter(pk=id).update(blocked=blocked, PayPass=pay_pass)
-            except Car.DoesNotExist:
-                ...
+            id = self.validate_int(id)
+            if id:
+                blocked = id in blocked_id
+                pay_pass = id in pay_pass_id
+                # print(id, blocked, pay_pass)
+                try:
+                    Car.objects.filter(pk=id).update(blocked=blocked, PayPass=pay_pass)
+                except Car.DoesNotExist:
+                    ...
 
         return redirect(reverse("cars:car_list"))
