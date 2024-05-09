07b998a866e16d7f55111f079ab8dc0ec153a8de
lexxai <lexxai@gmail.com>
2024-05-09 16:56:17 +0300

    poppler for pdf2image deploy solution and README

--- scripts/run_dev_app.cmd
+++ scripts/run_dev_app.cmd
@@ -5,9 +5,18 @@ echo "runing migration"
 
 call migrate_dev_app.cmd
 
+SETLOCAL
+SET "POPPER_BIN=..\LIBS\poppler\Library\bin"
+IF EXIST "%POPPER_BIN%" (
+    SET "PATH=%POPPER_BIN%;%PATH%"
+) ELSE (
+    echo Folder with 'LIBS\poppler\Library\bin' does not exist. PDF function is limited in app.
+)
 PUSHD "..\FRONTEND\fastparking"
-poetry env info -p
-echo "\Scripts\activate"
+REM poetry env info -p
+REM echo "\Scripts\activate"
 git rev-parse --short HEAD > ..\git-version.txt
+poetry shell
 python manage.py runserver 0.0.0.0:8000
 POPD
+ENDLOCAL
