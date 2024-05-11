3f2136912a5ac6592cb3d8343fed5b2fb386d9ba
lexxai <lexxai@gmail.com>
2024-05-11 17:27:27 +0300

    formatDateInString in JavaScript

--- FRONTEND/fastparking/parking/static/parking/js/local_datetime.js
+++ FRONTEND/fastparking/parking/static/parking/js/local_datetime.js
@@ -1,55 +1,57 @@
-function parse_utc_string(datetimeString){
-    // Example datetime string in "yyyy-mm-dd hh:mm:ss" format
-    const parts = datetimeString.split(/[- :]/);
-    const year = parseInt(parts[0]);
-    const month = parseInt(parts[1]) - 1; // Month is zero-indexed
-    const day = parseInt(parts[2]);
-    const hour = parseInt(parts[3]);
-    const minute = parseInt(parts[4]);
-    const second = parseInt(parts[5]);
-    return Date.UTC(year, month, day, hour, minute, second);
+const regexPattern_datetime = /^(.*)(\d{4}-\d{2}-\d{2} \d{2}:\d{2}(?::\d{2})?)(.*)$/;
+
+function parse_utc_string(datetimeString) {
+  // Example datetime string in "yyyy-mm-dd hh:mm:ss" format
+  const parts = datetimeString.split(/[- :]/);
+  const year = parseInt(parts[0]);
+  const month = parseInt(parts[1]) - 1; // Month is zero-indexed
+  const day = parseInt(parts[2]);
+  const hour = parseInt(parts[3]);
+  const minute = parseInt(parts[4]);
+  const second = parts[5] ? parseInt(parts[5]) : 0;
+  return Date.UTC(year, month, day, hour, minute, second);
 }
 
-function get_client_tz_offset(){
-    const date = new Date();
-    const offset = date.getTimezoneOffset();// * 60 * 1000; // Convert minutes offset to milliseconds
-    return offset
+function get_client_tz_offset() {
+  const date = new Date();
+  const offset = date.getTimezoneOffset(); // * 60 * 1000; // Convert minutes offset to milliseconds
+  return offset;
 }
 
-function parse_datetime_utc(){
-    // Get all table cells with class name "dateutc"
-    const cells = document.querySelectorAll('.datetime_utc');
-
-    // Loop through each cell and convert the datetime to the client's timezone
-    cells.forEach(function(cell) {
-        // Parse the datetime string from the cell
-        const datetimeString = cell.innerText;
-        if (datetimeString) {
-            const datetime = new Date(datetimeString+"Z");
-            // Convert the datetime to the client's timezone
-//            const clientTimeZoneDatetime = datetime.toLocaleString(undefined, {
-//                year: 'numeric',
-//                month: '2-digit',
-//                day: '2-digit',
-//                hour: 'numeric',
-//                minute: 'numeric',
-//                timeZoneName: 'short'
-//            });
-
-            const formatter = new Intl.DateTimeFormat([], {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-    //            timeZoneName: 'short'
-            });
-
-            const formattedDate = formatter.format(datetime);
-            // Update the cell with the datetime in the client's timezone
-    //        cell.innerText = clientTimeZoneDatetime;
-            cell.innerText = formattedDate;
-        }
-    });
+function format_datetime_to_local(datetime, show_seconds = false) {
+  const options = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  if (show_seconds) {
+    options.second = "2-digit";
+  }
+  const formatter = new Intl.DateTimeFormat([], options);
+  return formatter.format(datetime);
 }
-document.addEventListener("DOMContentLoaded", parse_datetime_utc)
+
+function formatDateInString(originalString) {
+  const match = originalString.match(regexPattern_datetime);
+  const text_start_Part = match ? match[1] : null;
+  const dateTimePart = match ? match[2] : null;
+  const text_end_Part = match ? match[3] : null;
+
+  const parsedDateTime = dateTimePart ? new Date(dateTimePart + "Z") : null;
+  const formattedDateTime = format_datetime_to_local(parsedDateTime);
+  const newString = formattedDateTime ? text_start_Part + formattedDateTime + text_end_Part : originalString;
+  return newString;
+}
+
+function parse_datetime_utc() {
+  const cells = document.querySelectorAll(".datetime_utc");
+  cells.forEach((cell) => {
+    const datetimeString = cell.innerText;
+    if (datetimeString) {
+      cell.innerText = formatDateInString(datetimeString);
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded", parse_datetime_utc);
