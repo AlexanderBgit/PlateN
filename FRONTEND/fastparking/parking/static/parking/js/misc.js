31b5b7a47f6a8e678cbe9c46f01a11a71f8034f8
lexxai <lexxai@gmail.com>
2024-05-12 19:52:02 +0300

    js clear form fo misc.js

--- FRONTEND/fastparking/parking/static/parking/js/misc.js
+++ FRONTEND/fastparking/parking/static/parking/js/misc.js
@@ -1,12 +1,24 @@
 
-  function printPage() {
+function printPage() {
     window.print();
-  }
+}
 
-  function printDiv(divName) {
+function printDiv(divName) {
     const printContents = document.getElementById(divName).innerHTML;
     const originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
-  }
+}
+
+function clear_form(fieldsToClear=['car_no', 'p_space']){
+    let form=document.getElementById('form_filter');
+    if (form) {
+      let inputs = form.getElementsByTagName('input');
+      for (let i = 0; i < inputs.length; i++) {
+        if (fieldsToClear.includes(inputs[i].name)) {
+          inputs[i].value = '';
+        }
+      }
+     }
+}
