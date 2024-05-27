23e6623cea0db0fcf4eb7adafd91a781ad5383a0
lexxai <lexxai@gmail.com>
2024-05-27 22:08:01 +0300

    copy demo users by button press

--- scripts/create_django_auto_admin.cmd
+++ scripts/create_django_auto_admin.cmd
@@ -3,6 +3,7 @@
 @REM poetry env info -p
 @REM echo "\Scripts\activate"
 PUSHD "../FRONTEND/fastparking"
+set PYTHONPATH=.
 poetry run python ./admin/create_groups.py
 poetry run python ./admin/create_admin_user.py
 POPD
