d4e58524832bad9d30e1ba04deb66e6f1381a06c
lexxai <lexxai@gmail.com>
2024-05-27 19:44:49 +0300

    Admin title

--- FRONTEND/fastparking/fastparking/urls.py
+++ FRONTEND/fastparking/fastparking/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
