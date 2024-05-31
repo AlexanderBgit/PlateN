9b2a0b96debb7ae5d8c673d5117bd6288e8661f9
lexxai <lexxai@gmail.com>
2024-05-31 23:07:30 +0300

    spell ,

--- BACKEND/api/main.py
+++ BACKEND/api/main.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any
 
-from fastapi import FastAPI, Path, Query, Depends, HTTPException, Request, status, 
+from fastapi import FastAPI, Path, Query, Depends, HTTPException, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -61,10 +61,10 @@ def url_x_for(
 
 def relative_url(request: Request, name: str, **path_params) -> str:
     absolute_url = str(request.url_for(name, **path_params))
-    return str(absolute_url.replace(str(request.base_url), '/'))
+    return str(absolute_url.replace(str(request.base_url), "/"))
 
 
-templates.env.filters['relative_url'] = relative_url
+templates.env.filters["relative_url"] = relative_url
 templates.env.globals["url_for"] = url_x_for
 
 
