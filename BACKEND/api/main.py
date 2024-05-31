03f79a23576e1619cb3c515f13ca962ca999b3b1
lexxai <lexxai@gmail.com>
2024-05-31 21:49:41 +0300

    urlx_for - x-forwarded-proto

--- BACKEND/api/main.py
+++ BACKEND/api/main.py
@@ -1,9 +1,11 @@
 import logging
+from typing import Any
 
 from fastapi import FastAPI, Path, Query, Depends, HTTPException, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from jinja2 import pass_context
 from fastapi.responses import RedirectResponse
 
 from conf.config import settings
@@ -39,6 +41,32 @@ templates = Jinja2Templates(directory="templates")
 app.include_router(main.router, prefix="/api/v1")
 app.include_router(plate.router, prefix="/api/v1")
 app.include_router(face_detection.router, prefix="/api/v1")
+
+
+def https_url_for(request: Request, name: str, **path_params) -> str:
+    http_url = request.url_for(name, **path_params)
+    # Replace 'http' with 'https'
+    return str(http_url.replace("http", "https", 1))
+
+
+@pass_context
+def urlx_for(
+    context: dict,
+    name: str,
+    **path_params: Any,
+) -> str:
+    request: Request = context["request"]
+    http_url = request.url_for(name, **path_params)
+    if scheme := request.headers.get("x-forwarded-proto"):
+        print(f"{scheme=}")
+        return http_url.replace(scheme=scheme)
+    print(f"{request.base_url=}")
+    # http_url = http_url.replace(scheme=scheme)
+    return http_url
+
+
+templates.env.globals["https_url_for"] = https_url_for
+templates.env.globals["url_for"] = urlx_for
 
 
 @app.get("/")
