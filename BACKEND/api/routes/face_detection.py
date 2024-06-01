7f74fa9bc336ffa0974e91544922bfe0b5224f02
lexxai <lexxai@gmail.com>
2024-06-02 02:51:41 +0300

    use ImageQueue, CascadeClassierFun

--- BACKEND/api/routes/face_detection.py
+++ BACKEND/api/routes/face_detection.py
@@ -1,15 +1,18 @@
 import logging
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, HTTPException
 from starlette.responses import JSONResponse
 
 
 from conf.config import settings
-from services.face_detection.cascade_classifier import face_detection_srv, startup_srv
+from services.face_detection.cascade_classifier import CascadeClassierFun
+from services.face_detection.image_queue import ImageQueue
 from services.services import model_load_status
 
 router = APIRouter(prefix="/face_detection", tags=["face_detection"])
 
 logger = logging.getLogger(f"{settings.app_name}.{__name__}")
+
+face_detection_image_queue: None | ImageQueue = None
 
 
 @router.websocket("")
@@ -18,8 +21,15 @@ async def face_detection(websocket: WebSocket):
     This is the endpoint that we will be sending request to from the
     frontend
     """
+
+    if face_detection_image_queue is None:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error not ready face_detection_image_queue.",
+        )
+
     try:
-        await face_detection_srv(websocket)
+        await face_detection_image_queue.loop(websocket)
     except Exception as e:
         # logger.error(e)
         ...
@@ -27,8 +37,10 @@ async def face_detection(websocket: WebSocket):
 
 @router.on_event("startup")
 async def startup():
+    global face_detection_image_queue
     """
     This tells fastapi to load the classifier upon app startup
     so that we don't have to wait for the classifier to be loaded after making a request
     """
-    await startup_srv()
+    cc_func = CascadeClassierFun()
+    face_detection_image_queue = ImageQueue(cc_func.load())
--- BACKEND/api/services/face_detection/cascade_classifier.py
+++ BACKEND/api/services/face_detection/cascade_classifier.py
@@ -11,72 +11,18 @@ from conf.config import settings
 logger = logging.getLogger(f"{settings.app_name}.{__name__}")
 
 
-# initialize the classifier that we will use
-cascade_classifier = cv2.CascadeClassifier()
+class CascadeClassierFun:
 
+    cascade_classifier: cv2.CascadeClassifier
 
-class Faces(BaseModel):
-    """This is a pydantic model to define the structure of the streaming data
-    that we will be sending the the cv2 Classifier to make predictions
-    It expects a List of a Tuple of 4 integers
-    """
+    def __init__(self):
+        self.cascade_classifier = cv2.CascadeClassifier()
 
-    faces: List[Tuple[int, int, int, int]]
+    def detect(self, gray):
+        return self.cascade_classifier.detectMultiScale(gray)
 
-
-async def receive(websocket: WebSocket, queue: asyncio.Queue):
-    """
-    This is the asynchronous function that will be used to receive webscoket
-    connections from the web page
-    """
-    bytes = await websocket.receive_bytes()
-    try:
-        queue.put_nowait(bytes)
-    except asyncio.QueueFull:
-        pass
-
-
-async def detect(websocket: WebSocket, queue: asyncio.Queue):
-    """
-    This function takes the received request and sends it to our classifier
-    which then goes through the data to detect the presence of a human face
-    and returns the location of the face from the continous stream of visual data as a
-    list of Tuple of 4 integers that will represent the 4 Sides of a rectangle
-    """
-    while True:
-        bytes = await queue.get()
-        data = np.frombuffer(bytes, dtype=np.uint8)
-        img = cv2.imdecode(data, 1)
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        faces = cascade_classifier.detectMultiScale(gray)
-        if len(faces) > 0:
-            faces_output = Faces(faces=faces.tolist())  # type: ignore
-        else:
-            faces_output = Faces(faces=[])
-        await websocket.send_json(faces_output.dict())
-
-
-async def face_detection_srv(websocket: WebSocket):
-    """
-    This is the endpoint that we will be sending request to from the
-    frontend
-    """
-    await websocket.accept()
-    queue: asyncio.Queue = asyncio.Queue(maxsize=10)
-    detect_task = asyncio.create_task(detect(websocket, queue))
-    try:
-        while True:
-            await receive(websocket, queue)
-    except WebSocketDisconnect:
-        detect_task.cancel()
-        await websocket.close()
-
-
-async def startup_srv():
-    """
-    This tells fastapi to load the classifier upon app startup
-    so that we don't have to wait for the classifier to be loaded after making a request
-    """
-    cl = cascade_classifier.load(
-        cv2.data.haarcascades + "haarcascade_frontalface_default.xml"  # type: ignore
-    )
+    def load(self):
+        self.cascade_classifier.load(
+            cv2.data.haarcascades + "haarcascade_frontalface_default.xml"  # type: ignore
+        )
+        return self.detect
