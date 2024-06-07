4271c904f73e179662cce162359d6463da636fcc
lexxai <lexxai@gmail.com>
2024-06-07 17:15:39 +0300

    bg for text

--- BACKEND/api/static/cam_client/js/cam_face_yn.js
+++ BACKEND/api/static/cam_client/js/cam_face_yn.js
@@ -46,25 +46,45 @@ function draw_landmarks(ctx, landmarks, scale) {
   }
 }
 
+
 function draw_box(ctx, scaledBox, titleText) {
   // Title text properties
   const titleFontSize = 16;
   const titleYOffset = 20; // Adjust offset for text placement
   // Draw the box
   ctx.strokeStyle = "#49fb35"; // Set stroke color
+  ctx.lineWidth = 5; // Set line thickness in pixels
   ctx.beginPath(); // Start a new path
   ctx.rect(scaledBox.x, scaledBox.y, scaledBox.width, scaledBox.height); // Define the rectangle
   ctx.stroke(); // Stroke the path (draw lines)
+
+  ctx.font = `${titleFontSize}px Arial`; // Set font style and size
+  const textMetrics = ctx.measureText(titleText); // Get text dimensions
+  const textWidth = textMetrics.width;
+  const textHeight = titleFontSize; // Assuming font height is equal to font size
+
+  // Adjust padding as needed (example: 5 pixels)
+  const padding = { x: 5, y: 5 }
+  const background = {
+   x: scaledBox.x + scaledBox.width / 2 - textWidth / 2 - padding.x,
+   y: scaledBox.y - titleYOffset - textHeight - padding.y,
+   width: textWidth + padding.x * 2,
+   height: textHeight + padding.y * 2,
+  }
+  ctx.fillStyle = "#000000A0"; // Set background color
+  ctx.fillRect(background.x, background.y, background.width, background.height);
+
 
   // Draw the title text
   ctx.font = `${titleFontSize}px Arial`; // Set font style and size
   ctx.fillStyle = ctx.strokeStyle; // Set text color
   ctx.fillText(
     titleText,
-    scaledBox.x + scaledBox.width / 2 - ctx.measureText(titleText).width / 2,
+    scaledBox.x + scaledBox.width / 2 - textWidth / 2,
     scaledBox.y - titleYOffset
   ); // Center text on top of the box
 }
+
 
 const draw_detected = (video, canvas, detected, scale = 1.0) => {
   const ctx = canvas.getContext("2d");
